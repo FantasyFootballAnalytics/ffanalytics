78d33a5d173a4bd351f6d0f522c8ec4c0e1ef7a7
RandalMorris <morrrand@gmail.com>
2020-08-25 13:47:09 -0500

    Update source_configs.R

--- data-raw/source_configs.R
+++ data-raw/source_configs.R
@@ -511,7 +511,7 @@ json_sites <- list(
   #### FantasyFootballNerd ####
   FantasyFootballNerd = list(
     base = "http://www.fantasyfootballnerd.com/service/",
-    api_key = "2tn5un7ws52w",
+    api_key = "test",
     get_path = function(season, week, position){
       data_type <- ifelse(week == 0, "draft", "weekly")
       week_no <- ifelse(week == 0, "", as.character(week))
