6f386b1a9e90e1ade27e3cc25528e9cb8038a311
Andrew Tungate <Andrew@Andrews-MacBook-Air-3.local>
2021-05-31 21:03:03 -0400

    removing janitor from dependencies

--- R/ffanalytics.R
+++ R/ffanalytics.R
@@ -1,9 +1,10 @@
-#' @import dplyr tibble tidyr purrr stringr httr janitor rvest
+#' @import dplyr tibble tidyr purrr stringr httr rvest
 #' @importFrom glue glue
 #' @importFrom lubridate year
 #' @importFrom data.table fread
 #' @importFrom Hmisc wtd.quantile
 #' @importFrom readxl read_xlsx
+#' @importFrom janitor clean_names
 .onLoad <- function(libname, pkgname){
   player_table <<- dplyr::as_tibble(
     data.table::fread("https://s3.us-east-2.amazonaws.com/ffanalytics/packagedata/player_table.csv",
