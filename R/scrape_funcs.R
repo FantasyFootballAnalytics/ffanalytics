4b9f4e08f5860a62b6bc27899cda25e6bf3879f2
Andrew Tungate <Andrew@Andrews-MacBook-Air-3.local>
2021-08-29 13:49:46 -0400

    fixing week 1 nfl scrape

--- R/scrape_funcs.R
+++ R/scrape_funcs.R
@@ -37,8 +37,7 @@ scrape_data <- function(
                    c("CBS", "ESPN", "FantasyData", "FantasyPros", "FantasySharks", "FFToday",
                      "FleaFlicker", "NumberFire", "Yahoo", "FantasyFootballNerd", "NFL",
                      "RTSports","Walterfootball"))
-  src_selfcont = match.arg(src, several.ok = TRUE,
-                           c("NFL", "CBS", "FantasySharks", "NumberFire", "Walterfootball"))
+  src_selfcont = intersect(src, c("NFL", "CBS", "FantasySharks", "NumberFire", "Walterfootball"))
   src = setdiff(src, src_selfcont)
   pos <- match.arg(pos, several.ok = TRUE,
                    c("QB", "RB", "WR", "TE", "K", "DST", "DL", "LB", "DB"))
