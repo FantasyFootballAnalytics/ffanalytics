44f300a80ca5864f8bdccdb1c350ab953405b64b
Andrew Tungate <atungate@gmail.com>
2022-05-17 19:41:30 -0400

    updated schedule functions, need to cleanup the kickoff pull

--- R/schedule_data.R
+++ R/schedule_data.R
@@ -1,39 +1,72 @@
 
-schedule_url <- "https://api.myfantasyleague.com/2021/export?TYPE=nflSchedule&W=ALL&JSON=1"
-
-schedule_data <- httr::GET(schedule_url) %>%
-  httr::content() %>%
-  .[[c("fullNflSchedule", "nflSchedule")]] %>%
-  purrr:::keep(~ "matchup" %in% names(.))
-
-names(schedule_data) <- paste0("week_", seq_len(length(schedule_data)))
-
-schedule_data <- purrr::map(schedule_data, ~ `names<-`(.x$matchup, paste0("match_", seq_along(.x$matchup))))
-
-schedule_data$week_22$match_1$kickoff = schedule_data$week_22$match_1
-schedule_data$week_22$match_2 = NULL
-schedule_data$week_22$match_3 = NULL
-
-# The nesting structure does not work well with the superbowl, manually added below as "last" game
-first_last_games <- purrr::modify_depth(schedule_data, 2, `[[`, "kickoff") %>%
-  purrr::map(unlist, use.name = FALSE) %>%
-  purrr::map(as.numeric) %>%
-  purrr::map(summary) %>%
-  purrr::map(`[`, c("Min.", "Max.")) %>%
-  purrr::map(~ as.POSIXct(as.numeric(.x), origin = "1970-01-01")) %>%
-  purrr::map(`names<-`, c("first", "last"))
-
-scrape_start_date <- first_last_games %>%
-  purrr::map_chr(`[[`, "last") %>%
-  lag %>%
-  as.numeric() %>%
-  as.POSIXct(origin = "1970-01-01") %>%
-  as.Date()
-
-scrape_start_date[1] <- as.Date(format(first_last_games$week_1[["first"]], format = "%Y-%m-%d")) - 7L
-
-scrape_start_date[19:22] <- scrape_start_date[18] + c(7, 14, 21, 35)
-
-scrape_week <- function(){
-  length(which(Sys.Date() >= scrape_start_date))
+get_scrape_year <- function(date) {
+  if(missing(date)) {
+    date = Sys.Date()
   }
+  date = as.POSIXlt(date)
+  cal_year = date$year + 1900L
+  cal_month = date$mon + 1L
+
+  if(cal_month %in% 1:3) {
+    cal_year - 1L
+  } else {
+    cal_year
+  }
+}
+
+get_first_last_kickoff <- function(year) {
+  if(missing(year)) {
+    year = get_scrape_year()
+  }
+  if(!is.numeric(year)) {
+    stop("year must be numeric")
+  }
+
+  schedule_url = sprintf("https://api.myfantasyleague.com/%d/export?TYPE=nflSchedule&W=ALL&JSON=1",
+                         year)
+
+  req_body = httr2::request(schedule_url) %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_json()
+
+  req_schedule = req_body[[c("fullNflSchedule", "nflSchedule")]]
+  req_schedule = req_schedule[sapply(req_schedule, function(x) any(names(x) == "matchup"))]
+  names(req_schedule) = paste0("week_", seq_len(length(req_schedule)))
+  first_last_games = lapply(req_schedule, function(x) {
+    kick_unixtimes = sapply(x$matchup, `[[`, "kickoff")
+    kick_unixtimes = as.integer(kick_unixtimes)
+    first_last = c(first = min(kick_unixtimes), last = max(kick_unixtimes))
+    as.POSIXct(first_last, origin = "1970-01-01")
+  })
+  first_last_games
+
+}
+
+get_scrape_starts = function(first_last_games) {
+  if(missing(first_last_games)) {
+    first_last_games = get_first_last_kickoff()
+  }
+  first_last_df = dplyr::bind_rows(first_last_games)
+  last_vec = first_last_df$last
+  last_vec = as.Date(substr(last_vec, 1, 10)) + 1L
+  last_vec = dplyr::lag(last_vec)
+  last_vec[1] = as.Date(first_last_df$first[1]) - 7L
+  last_vec[19:22] = last_vec[18] + c(7, 14, 21, 35)
+  last_vec
+}
+get_scrape_week = function(scrape_start_date) {
+  if(missing(scrape_start_date)) {
+    scrape_start_date = get_scrape_starts()
+  }
+  sum(Sys.Date() >= scrape_start_date)
+}
+
+first_last_games = get_first_last_kickoff()
+scrape_start_date = get_scrape_starts(first_last_games)
+
+
+# rrapply::rrapply(req_body,
+#                 function(x, .xname) .xname == "kickoff",
+#                 how = "prune")
+
+
